75159c1965fb9f4ab5820a5d564af9b7bb67a0f3
LAPTOP-I3RBO649\Andreu <andreu.offing@gmail.com>
2018-05-03 17:35:47 +0200

    reparacion Contact

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -523,22 +523,22 @@
       </div>
         
     <!-- Main JS  -->
-    <script type="text/javascript" src="assets/js/jquery-min.js"></script>      
-    <script type="text/javascript" src="assets/js/bootstrap.min.js"></script>    
-    <script type="text/javascript" src="assets/js/material.min.js"></script>
-    <script type="text/javascript" src="assets/js/material-kit.js"></script>
-    <script type="text/javascript" src="assets/js/jquery.parallax.js"></script>
-    <script type="text/javascript" src="assets/js/owl.carousel.min.js"></script>
-    <script type="text/javascript" src="assets/js/jquery.slicknav.js"></script>
-    <script type="text/javascript" src="assets/js/main.js"></script>
-    <script type="text/javascript" src="assets/js/jquery.counterup.min.js"></script>
-    <script type="text/javascript" src="assets/js/waypoints.min.js"></script>
-    <script type="text/javascript" src="assets/js/jasny-bootstrap.min.js"></script>
-    <script type="text/javascript" src="assets/js/bootstrap-select.min.js"></script>
-    <script type="text/javascript" src="assets/js/form-validator.min.js"></script>
-    <script type="text/javascript" src="assets/js/contact-form-script.js"></script>    
-    <script type="text/javascript" src="assets/js/jquery.themepunch.revolution.min.js"></script>
-    <script type="text/javascript" src="assets/js/jquery.themepunch.tools.min.js"></script>
+    <script type="text/javascript" src="{{ asset('/js/jquery-min.js') }}"></script>      
+    <script type="text/javascript" src="{{ asset('/js/bootstrap.min.js') }}"></script>    
+    <script type="text/javascript" src="{{ asset('/js/material.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('/js/material-kit.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('/js/jquery.parallax.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('/js/owl.carousel.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('/js/jquery.slicknav.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('/js/main.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('/js/jquery.counterup.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('/js/waypoints.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('/js/jasny-bootstrap.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('/js/bootstrap-select.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('/js/form-validator.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('/js/contact-form-script.js') }}"></script>    
+    <script type="text/javascript" src="{{ asset('/js/jquery.themepunch.revolution.min.js') }}"></script>
+    <script type="text/javascript" src="{{ asset('/js/jquery.themepunch.tools.min.js') }}"></script>
 
       <!-- Google Maps API -->
       <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyAHo_WtZ2nIYCgCLf7sINZaqcrpqSDio9o"></script>
