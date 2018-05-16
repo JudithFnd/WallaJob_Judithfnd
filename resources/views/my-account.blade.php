462c84aab0ac5e92314845eff8d161f54eaf38b2
Nitka <judithfnd20@gmail.com>
2018-05-16 11:09:37 +0200

    @include Footer a index.blade.php

--- resources/views/my-account.blade.php
+++ resources/views/my-account.blade.php
@@ -213,7 +213,7 @@
       <section class="section text-center" >
       <div class="container">
                 <!-- Start Animations Text -->
-                <form id="form_login" action="/action_page.php">
+                <form id="form_login" action="{{ route('logIn') }}">
                   <h1>Login</h1>
                   <p>Please fill in this form to login.</p>
                   <hr>
@@ -221,30 +221,21 @@
                   </div>
 
                   <div class="container">
-                    <label for="uname"><b>Username</b></label>
-                    <input type="text" placeholder="Enter Username" name="uname" required>
+                    <label for="email"><b>Email</b></label><br>
+                    <input type="email" placeholder="Enter Username" name="email" required><br>
 
-                    <label for="psw"><b>Password</b></label>
-                    <input type="password" placeholder="Enter Password" name="psw" required>
+                    <label for="password"><b>Password</b></label>
+                    <input type="password" placeholder="Enter Password" name="password" required>
                         
                     <button type="submit" class="btn btn-common btn-large">Login</button>
-                    <label>
-                      <input type="checkbox" checked="checked" name="remember"> Remember me
-                    </label>
+
                   </div>
 
-                  <div class="container" style="background-color:#f1f1f1">
-                    <span class="psw">Forgot <a href="#">password?</a></span>
                   </div>
                 </form>
-              
-                
-                
         </div>
       </div>
     </section>  
-
-     
 
           <!-- Footer Section Start -->
     <footer>
